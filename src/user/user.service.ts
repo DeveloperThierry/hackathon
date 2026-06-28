30bbc587778f4981e6d76a7804bb162abc524b3d
developerthierry <developerthierry@gmail.com>
2026-06-28 04:44:50 +0000

    Handle User Not Found Exception

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { LoggerService } from './user.logger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -27,7 +27,9 @@ export class UserService {
 
   findOneUser(id:number){
     this.logger.log(`Finding user ${id}`)
-    return this.users.filter(user => user.id == id) ?? null
+    const user = this.users.find(user => user.id == id)
+    if(!user) throw new NotFoundException("User not found")
+    return user
   }
 
   createUser(dto:CreateUserDto){
