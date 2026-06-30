468f8113f9cb61a10c7a535d4e6e8bcfd560dcce
developerthierry <developerthierry@gmail.com>
2026-06-30 16:16:01 +0000

    Add Interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-
+import { TransformInterceptor } from './utils/transform.interceptor'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TransformInterceptor())
   const port = process.env.PORT ?? 3000
   await app.listen(port);
-  // Logger.log(`Application is running on: http://localhost:${port}`, 'Bootstrap');
+  Logger.log(`Application is running on: http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
